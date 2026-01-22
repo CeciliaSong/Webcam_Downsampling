f46f6aa654e8ca2439f25f4134cd7c2dc893e9ba
Cecilia <ssyjh14@nottingham.edu.cn>
2026-01-22 13:43:36 +0800

    debug: fix the issue of cannot reach 25fps

--- src/image_saver.cpp
+++ src/image_saver.cpp
@@ -78,5 +78,5 @@ int main(int argc, char * argv[])
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<ImageSaver>());
     rclcpp::shutdown();
-    return 0;
+    return 0; //terminate main
 }
